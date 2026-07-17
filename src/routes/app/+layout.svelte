407a0853999248d19e79bd90906d53a05a155c56
taoki <84th.tccaoki@gmail.com>
2026-07-17 17:21:42 +0900

    style: サイドバーのデザインを更新

--- src/routes/app/+layout.svelte
+++ src/routes/app/+layout.svelte
@@ -77,73 +77,78 @@
 			class="min-h-screen bg-white p-4"
 			style="border-right: 0.5px solid var(--small-text-color);"
 		>
-			<div class="hover:bg-(--small-text-color) transition p-1 rounded-xl cursor-pointer">
-				<a href="/app/home" class="flex shrink-0 whitespace-nowrap transition w-fit">
-					<img
-						src="https://cdn.atserver186.jp/img/material/ats-logo.webp"
-						alt="ATSERVER Logo"
-						class="h-10"
-					/>
-					<p class="text-(--main-text-color) m-auto ml-2 font-bold">Comina Link</p></a
-				>
-			</div>
-			<ul>
-				<hr class="m-hr" />
-				<li
-					class="hover:bg-(--small-text-color) transition p-1 rounded-xl cursor-pointer my-4 text-lg font-thin"
-				>
-					<a href="/app/home"><i class="fa-solid fa-house mr-1 text-bace"></i>ホーム</a>
-				</li>
-				<li
-					class="hover:bg-(--small-text-color) transition p-1 rounded-xl cursor-pointer my-4 text-lg font-thin"
-				>
-					<a href="/app/links"><i class="fa-solid fa-link mr-1 text-bace"></i>リンク設定</a>
-				</li>
-				<li
-					class="hover:bg-(--small-text-color) transition p-1 rounded-xl cursor-pointer my-4 text-lg font-thin"
-				>
-					<a href="/app/posts"><i class="fa-solid fa-circle-plus mr-1 text-bace"></i>投稿管理</a>
-				</li>
-				<li
-					class="hover:bg-(--small-text-color) transition p-1 rounded-xl cursor-pointer my-4 text-lg font-thin"
-				>
-					<a href="/app/design"><i class="fa-solid fa-pen-fancy mr-1 text-bace"></i>デザイン設定</a>
-				</li>
-				<li
-					class="hover:bg-(--small-text-color) transition p-1 rounded-xl cursor-pointer my-4 text-lg font-thin"
-				>
-					<a href="/app/media"
-						><i class="fa-solid fa-pen-fancy mr-1 text-bace"></i>メディアライブラリ</a
+			<div class="flex flex-col h-full">
+				<div class="hover:bg-(--main-hover-color) transition p-1 rounded-xl cursor-pointer">
+					<a href="/app/home" class="flex shrink-0 whitespace-nowrap transition w-fit">
+						<img
+							src="https://cdn.atserver186.jp/img/material/ats-logo.webp"
+							alt="ATSERVER Logo"
+							class="h-10"
+						/>
+						<p class="text-(--main-text-color) m-auto ml-2 font-bold">Comina Link</p></a
 					>
-				</li>
-				<li
-					class="hover:bg-(--small-text-color) transition p-1 rounded-xl cursor-pointer my-4 text-lg font-thin"
-				>
-					<a href={publicPageUrl}
-						><i class="fa-solid fa-square-arrow-up-right mr-1 text-bace"></i>あなたの公開ページ</a
+				</div>
+				<ul>
+					<hr class="m-hr" />
+					<li
+						class="hover:bg-(--main-hover-color) transition p-1 rounded-xl cursor-pointer my-4 text-lg font-thin"
 					>
-				</li>
-			</ul>
-			<div class="mt-auto">
-				<!--s: account-info-->
-				<button
-					type="button"
-					onclick={() => openModal('account_info')}
-					class="cursor-pointer w-full hover:bg-(--small-text-color) transition rounded-xl p-1"
-				>
-					<div class="flex">
-						<div>
-							<img
-								src={accountAvatarUrl}
-								alt="{accountDisplayName}のアイコン"
-								class="h-10 rounded-full"
-							/>
+						<a href="/app/home"><i class="fa-solid fa-house mr-1 text-bace"></i>ホーム</a>
+					</li>
+					<li
+						class="hover:bg-(--main-hover-color) transition p-1 rounded-xl cursor-pointer my-4 text-lg font-thin"
+					>
+						<a href="/app/links"><i class="fa-solid fa-link mr-1 text-bace"></i>リンク設定</a>
+					</li>
+					<li
+						class="hover:bg-(--main-hover-color) transition p-1 rounded-xl cursor-pointer my-4 text-lg font-thin"
+					>
+						<a href="/app/posts"><i class="fa-solid fa-circle-plus mr-1 text-bace"></i>投稿管理</a>
+					</li>
+					<li
+						class="hover:bg-(--main-hover-color) transition p-1 rounded-xl cursor-pointer my-4 text-lg font-thin"
+					>
+						<a href="/app/design"
+							><i class="fa-solid fa-pen-fancy mr-1 text-bace"></i>デザイン設定</a
+						>
+					</li>
+					<li
+						class="hover:bg-(--main-hover-color) transition p-1 rounded-xl cursor-pointer my-4 text-lg font-thin"
+					>
+						<a href="/app/media"
+							><i class="fa-solid fa-images mr-1 text-bace"></i>メディアライブラリ</a
+						>
+					</li>
+					<li
+						class="hover:bg-(--main-hover-color) transition p-1 rounded-xl cursor-pointer my-4 text-lg font-thin"
+					>
+						<a href={publicPageUrl}
+							><i class="fa-solid fa-square-arrow-up-right mr-1 text-bace"></i>あなたの公開ページ</a
+						>
+					</li>
+				</ul>
+
+				<div class="mt-auto">
+					<!--s: account-info-->
+					<button
+						type="button"
+						onclick={() => openModal('account_info')}
+						class="cursor-pointer w-full hover:bg-(--main-hover-color) transition rounded-xl p-1"
+					>
+						<div class="flex">
+							<div>
+								<img
+									src={accountAvatarUrl}
+									alt="{accountDisplayName}のアイコン"
+									class="h-10 rounded-full"
+								/>
+							</div>
+							<div class="my-auto ml-2">{accountDisplayName}</div>
+							<i class="fa-solid fa-chevron-right my-auto ml-auto"></i>
 						</div>
-						<div class="my-auto ml-2">{accountDisplayName}</div>
-						<i class="fa-solid fa-chevron-right my-auto ml-auto"></i>
-					</div>
-				</button>
-				<!--e: account-info-->
+					</button>
+					<!--e: account-info-->
+				</div>
 			</div>
 		</aside>
 		<div class="main-content-area">
