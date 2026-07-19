f199de00a2ffddcacf4de70e354e20018a3bcb36
taoki <84th.tccaoki@gmail.com>
2026-07-19 10:25:04 +0900

    style: ユーザ公開ページのスタイルを更新

--- src/routes/dummy_profile/+page.svelte
+++ src/routes/dummy_profile/+page.svelte
@@ -11,75 +11,95 @@
 
 <main class="main-content">
 	<section class="profile-section">
-		<div class="profile-title">
-			<img
-				src="{data.serviceIconUrlLong}"
-				alt="{data.serviceName}"
-				style="width:100px;max-width:90vw;height:auto;margin-bottom:8px;"
-			/>
+    <!--s: service-ad-->
+		<img
+			src={data.serviceIconUrlLong}
+			alt={data.serviceName}
+			style="width:100px;max-width:90vw;height:auto;margin-bottom:8px;"
+		/>
+        <!--e: service-ad-->
+        <!---->
+		<img
+			class="profile-avatar"
+			src={data.accountAvatarUrl}
+			alt="{data.accountDisplayName}のアイコン"
+		/>
+		<!--s: user-text-->
+		<div class="profile-text">
+			<h3 class="profile-display-name">{data.accountDisplayName}</h3>
+			<div class="profile-bio">
+				<span>bio texts</span>
+			</div>
 		</div>
-		<img class="profile-avatar" src="{data.accountAvatarUrl}" alt="{data.accountDisplayName}のアイコン" />
-		<div class="profile-username">{data.accountDisplayName}</div>
-		<div class="profile-bio">
-			bio
-		</div>
+		<!--e: user-text-->
+		<!---->
+		<!--e: user-link-icons-->
 		<div class="profile-icons">
 			<a href="https://atserver186.jp" class="icon-link" title="ATSERVER" target="_blank"
 				><img src="https://cdn.atserver186.jp/img/material/ats-logo.webp" alt="ATSERVER" /></a
 			>
+			<a href="https://atserver186.jp" class="icon-link" title="ATSERVER" target="_blank"
+				><img src="https://cdn.atserver186.jp/img/material/ats-logo.webp" alt="ATSERVER" /></a
+			>
+			<a href="https://atserver186.jp" class="icon-link" title="ATSERVER" target="_blank"
+				><img src="https://cdn.atserver186.jp/img/material/ats-logo.webp" alt="ATSERVER" /></a
+			>
+			<a href="https://atserver186.jp" class="icon-link" title="ATSERVER" target="_blank"
+				><img src="https://cdn.atserver186.jp/img/material/ats-logo.webp" alt="ATSERVER" /></a
+			>
 		</div>
+		<!--e: user-link-icons-->
+		<!---->
+		<!--s: user-links-->
 		<div class="profile-links">
-			<a class="profile-link-card" href="https://atserver186.jp" target="_blank"
-				><img src="https://cdn.atserver186.jp/img/material/ats-logo.webp" alt="ATSERVER" />ATSERVER<br
-				/><span>ホームページ</span></a
-			>
-			<div
-				style="display:flex;flex-direction:column;align-items:center;justify-content:center;margin-top:24px;"
-			>
-				<a href="https://link.atserver186.jp" target="_blank"
-					><img
-						src="https://pic.atserver186.jp/img/atserver/header_logo.webp"
-						alt="ATSLink"
-						style="width:70px;max-width:90vw;height:auto;margin-bottom:8px;"
-					/></a
-				><a
-					href="{data.serviceDomain}"
-					style="font-size:0.8rem;color:black;text-decoration:none;margin-bottom:4px;"
-					target="_blank">Created by {data.serviceName}</a
-				>
-				<p style="font-size:0.8rem;margin:0;">{data.serviceName}. All Rights Reserved</p>
-			</div>
+			<a class="profile-link-card" href="https://atserver186.jp" target="_blank">
+				<img src="https://cdn.atserver186.jp/img/material/ats-logo.webp" alt="ATSERVER" />
+				ATSERVER
+				<br />
+				<span>ホームページ</span>
+			</a>
 		</div>
+		<!--e: user-links-->
+		<!---->
+		<!--s: service-ad-->
+		<div
+			style="display:flex;flex-direction:column;align-items:center;justify-content:center;margin-top:24px;"
+		>
+			<a href="https://link.atserver186.jp" target="_blank"
+				><img
+					src="https://pic.atserver186.jp/img/atserver/header_logo.webp"
+					alt="ATSLink"
+					style="width:70px;max-width:90vw;height:auto;margin-bottom:8px;"
+				/></a
+			><a
+				href={data.serviceDomain}
+				style="font-size:0.8rem;color:black;text-decoration:none;margin-bottom:4px;"
+				target="_blank">Created by {data.serviceName}</a
+			>
+			<p style="font-size:0.8rem;margin:0;">{data.serviceName}. All Rights Reserved</p>
+		</div>
+		<!--e: service-ad-->
 	</section>
 </main>
 
 <style>
 	.main-content {
-		flex: 1;
 		display: flex;
+		flex: 1;
 		justify-content: center;
-		align-items: flex-start;
-		padding: 48px 0 0 0;
 	}
 
 	.profile-section {
-		background: #fff;
+		background-color: #ffffff;
 		border-radius: 18px;
 		box-shadow: 0 2px 16px rgba(0, 0, 0, 0.07);
-		padding: 40px 32px 32px 32px;
+		padding: 2rem 1rem;
 		max-width: 480px;
 		width: 100%;
 		margin-top: 32px;
 		display: flex;
 		flex-direction: column;
 		align-items: center;
-	}
-	.profile-title {
-		font-family: 'Segoe UI', sans-serif;
-		font-size: 1.5rem;
-		font-weight: 600;
-		margin-bottom: 12px;
-		color: #444;
 	}
 
 	.profile-avatar {
@@ -90,15 +110,23 @@
 		border: 4px solid #e6e6e6;
 		margin-bottom: 10px;
 	}
-	.profile-username {
+
+	.profile-text {
+		display: flex;
+		flex-direction: column;
+	}
+
+	.profile-display-name {
 		font-size: 1.2rem;
 		font-weight: bold;
-		margin-bottom: 8px;
+		margin-inline: auto;
 	}
+
 	.profile-icons {
 		display: flex;
 		gap: 16px;
-		margin-bottom: 18px;
+		margin-bottom: 1rem;
+		margin-top: 1rem;
 	}
 	.icon-link img {
 		width: 28px;
@@ -126,9 +154,7 @@
 		color: #222;
 		text-decoration: none;
 		box-shadow: 0 1px 4px rgba(0, 0, 0, 0.04);
-		transition:
-			background 0.2s,
-			box-shadow 0.2s;
+		transition: background-color 0.2s;
 		gap: 16px;
 	}
 	.profile-link-card img {
@@ -142,7 +168,18 @@
 		margin-left: 8px;
 	}
 	.profile-link-card:hover {
-		background: #e6e6ff;
+		background: #b6b6b8;
 		box-shadow: 0 2px 8px rgba(108, 99, 255, 0.08);
+	}
+
+	@media (max-width: 768px) {
+		.main-content {
+			min-height: 100vh;
+		}
+
+		.profile-section {
+			background-color: var(--main-bg-color);
+			box-shadow: none;
+		}
 	}
 </style>
