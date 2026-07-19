b6a5e787c866c123be9cc697944b58e8d3c9c55d
taoki <84th.tccaoki@gmail.com>
2026-07-19 23:06:48 +0900

    feat: 新規登録ページのUIを実装

--- src/routes/auth/login/+page.svelte
+++ src/routes/auth/login/+page.svelte
@@ -1,1 +1,14 @@
+<script>
+	const { data } = $props();
+
+	import { onMount } from 'svelte';
+
+	let pageTitle = 'ログイン';
+</script>
+
+<svelte:head>
+	<title>{pageTitle} | {data.serviceName}</title>
+	<meta property="og:title" content="{data.accountDisplayName} | {data.serviceName}" />
+</svelte:head>
+
 <p>ログイン</p>
