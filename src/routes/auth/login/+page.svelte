32cc0fd5081b4aaf6f08eeccc9031dc37b5ac35f
taoki <84th.tccaoki@gmail.com>
2026-07-19 23:09:05 +0900

    feat: ログインページのUIを実装

--- src/routes/auth/login/+page.svelte
+++ src/routes/auth/login/+page.svelte
@@ -11,4 +11,35 @@
 	<meta property="og:title" content="{data.accountDisplayName} | {data.serviceName}" />
 </svelte:head>
 
-<p>ログイン</p>
+<div class="flex mb-4">
+	<a
+		href="/auth"
+		class="mr-auto bg-(--emphasis-text-color) py-2 px-4 rounded-4xl transition-all hover:bg-(--sub-hover-color)"
+		style="border: 0.5px solid var(--small-text-color);"
+		title="戻る"
+	>
+		<i class="fa-solid fa-arrow-left"></i>
+	</a>
+</div>
+<label class="mb-4">
+	<div class="mb-1 flex items-end justify-between">
+		<span class="text-xl">メールアドレス</span>
+		<span class="text-lg font-normal text-gray-500">(例: &nbsp;hogehoge@example.com)</span>
+	</div>
+	<input type="text" class="w-full rounded-3xl outline-none appearance-none" />
+</label>
+<label class="mb-4">
+	<div class="mb-1 flex items-end justify-between">
+		<span class="text-xl">パスワード</span>
+	</div>
+	<input type="text" class="w-full rounded-3xl outline-none appearance-none" />
+</label>
+<div class="flex">
+	<a
+		href="/"
+		class="ml-auto bg-(--emphasis-text-color) py-2 px-4 rounded-4xl transition-all hover:bg-(--sub-hover-color)"
+		style="border: 0.5px solid var(--small-text-color);"
+	>
+		続ける<i class="fa-solid fa-arrow-right ml-1"></i>
+	</a>
+</div>
