f26537bf4526a336324c6354c34d4309ec56699b
taoki <84th.tccaoki@gmail.com>
2026-07-20 16:15:26 +0900

    feat: 認証系ページをさらにレスポンシブに対応

--- src/routes/auth/+layout.svelte
+++ src/routes/auth/+layout.svelte
@@ -28,13 +28,13 @@
 
 <main>
 	<section class="grid min-h-screen md:grid-cols-2">
-		<div class="mt-auto mx-auto md:ml-auto md:mr-50 md:my-auto">
+		<div class="mt-auto mx-auto xl:mr-50 md:ml-auto md:mx-auto md:my-auto">
 			<a href="/" class="flex shrink-0 whitespace-nowrap transition w-fit" title="ホーム">
 				<img src={data.serviceIconUrlShort} alt="ATSERVER Logo" class="h-10" />
 				<p class="text-(--main-text-color) m-auto ml-2 font-bold text-2xl">{data.serviceName}</p>
 			</a>
 		</div>
-		<div class="flex flex-col p-8 w-full h-100 mb-auto mx-auto md:h-auto md:w-[50%] md:mr-auto md:ml-50 md:my-auto">
+		<div class="flex flex-col p-8 w-full max-w-125 h-100 mb-auto mx-auto xl:ml-50 xl:w-[50%] md:h-auto md:w-full md:mr-auto md:mx-auto md:my-auto">
 			{@render children()}
 		</div>
 	</section>
