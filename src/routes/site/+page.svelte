8f71c69f7788e80634fd1a613dba402194c0ab9b
taoki <84th.tccaoki@gmail.com>
2026-07-29 23:39:22 +0900

    feat(sihome): 最下部の宣伝セクションにフォームを設置

--- src/routes/site/+page.svelte
+++ src/routes/site/+page.svelte
@@ -24,6 +24,18 @@
 		<p>横流し→/←</p>
 	</section>
 	<section class="my-10 w-[80%] bg-gray-200 mx-auto rounded-3xl p-2">
-		<p>登録して始める的な</p>
+		<p>登録して始める的なもの</p>
+		<form class="flex justify-between items-center overflow-hidden rounded-4xl w-[70%] mx-auto" action="/login/" method="GET">
+			<input
+				class="w-full h-12 px-3 py-3.75 border-none box-border text-[1em] outline-0"
+				type="text"
+				id="searchTerm"
+				name="search"
+				placeholder="{data.serviceDomain}/あなたの名前"
+			/>
+			<button class="flex justify-center items-center w-25 h-12 border-0 bg-amber-200 cursor-pointer" type="submit" title="検索する"
+				><i class="fas fa-search"></i></button
+			>
+		</form>
 	</section>
 </main>
