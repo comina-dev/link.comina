9864160277c2996b4f4390efe9f80879df22cf77
taoki <84th.tccaoki@gmail.com>
2026-07-29 23:08:04 +0900

    feat(sihome): 大まかなレイアウト決め

--- src/routes/site/+page.svelte
+++ src/routes/site/+page.svelte
@@ -6,9 +6,24 @@
 
 <svelte:head>
 	<title>{data.serviceName}</title>
-	<meta property="og:title" content="{data.serviceName}" />
+	<meta property="og:title" content={data.serviceName} />
 </svelte:head>
 
-<main class="mt-15 mr-1 ml-1 min-h-screen">
-	<p>test</p>
+<main>
+	<section class="min-h-screen bg-amber-200">
+		<p>test</p>
+	</section>
+	<section class="my-10 w-[80%] bg-gray-200 mx-auto rounded-3xl p-2">
+		<p>サービス紹介/qa</p>
+	</section>
+	<section class="my-10 w-[80%] bg-gray-200 mx-auto rounded-3xl p-2">
+		<p>お知らせ</p>
+	</section>
+	<section class="my-10 w-full bg-gray-200 mx-auto p-2">
+		<p>登録ユーザ表示</p>
+		<p>横流し→/←</p>
+	</section>
+	<section class="my-10 w-[80%] bg-gray-200 mx-auto rounded-3xl p-2">
+		<p>登録して始める的な</p>
+	</section>
 </main>
